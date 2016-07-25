1ebefba25275d345b266e765494b94a0593d2ce3
Iliyan Betovski <ibetovski@gmail.com>
2016-07-25 17:39:45 +0300

    Throws errors for the user's input

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,13 +8,15 @@ module.exports = function(grunt) {
         dest: './output/manamana1111111.scss',
         // property name to read from. It will be used for the css class name
         //  and for the css @keyframes name.
-        namespace: 'dodo',
+        // the namespace should contain valid css class names characters
+        namespace: 'custom-namespace-name',
         // we need the size of the animated element in order to calculate the translate values
         elementSize: {
           width: 100,
           height: 100
         },
         // which sass mixin to @include (this should be  created by the user)s
+        //  the default is 'curved-animation'
         sassMixin: 'sassence',
       }
     }
--- tasks/path-to-animation.js
+++ tasks/path-to-animation.js
@@ -3,31 +3,60 @@ var _ = require('lodash');
 
 module.exports = function(grunt) {
 
+  function logError(error) {
+    grunt.fail.fatal('path-to-animation: ' + error);
+  }
+
+  function throwForMissingConfig(property) {
+    logError(property + ' is missing in the task configuration');
+  }
+
   // the defaults
   grunt.config.merge({
     'path-to-animation': {
       options: {
-        src: './animation-paths.json'
+        src: './animation-paths.json',
+        'sassMixin': 'curved-animation'
       }
     }
   });
 
+  // Used to check if the namespace is a valid name for css classname to be used
+  var validCssClassNameRule = /^[a-z\-\_0-9]+$/g;
+
   grunt.registerMultiTask('path-to-animation', '', function() {
-      var config = _.assign({}, this.options(), this.data);
+    var config = _.assign({}, this.options(), this.data);
 
-      var pathsFile = JSON.parse(grunt.file.read(config.src, {encoding: 'utf8'}));
+    // error handling
+    if (typeof config.dest != 'string') {
+      throwForMissingConfig('dest');
+    }
 
-      if (!pathsFile[config.namespace]) {
-          logError('Can not find ' + config.namespace + ' in ' + config.src + ' file');
-      }
+    if (typeof config.namespace != 'string') {
+      throwForMissingConfig('namespace');
+    } else if (!validCssClassNameRule.test(config.namespace)) {
+      logError('namespace should contain valid css characters');
+    }
 
-      var paths = pathsFile[config.namespace];
+    if (typeof config.elementSize === 'undefined') {
+      throwForMissingConfig('elementSize');
+    } else if (typeof config.elementSize.width === 'undefined' || typeof config.elementSize.height === 'undefined') {
+      throwForMissingConfig('elementSize.width or elementSize.height');
+    }
 
-      // flattens all layouts
-      var fileContent = brain.generateCss(paths, config.namespace, config.elementSize.width, config.elementSize.height, config.sassMixin);
+    var pathsFile = JSON.parse(grunt.file.read(config.src, {encoding: 'utf8'}));
 
-      grunt.file.write(config.dest, fileContent, {encoding: 'utf8'});
-      grunt.log.write(config.dest + ' was created');
+    if (!pathsFile[config.namespace]) {
+      logError('Can not find ' + config.namespace + ' in ' + config.src + ' file');
+    }
+
+    var paths = pathsFile[config.namespace];
+
+    // flattens all layouts
+    var fileContent = brain.generateCss(paths, config.namespace, config.elementSize.width, config.elementSize.height, config.sassMixin);
+
+    grunt.file.write(config.dest, fileContent, {encoding: 'utf8'});
+    grunt.log.write(config.dest + ' was created');
   });
 
 }
